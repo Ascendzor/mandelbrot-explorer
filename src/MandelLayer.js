0202500140f4d8f01b7e8cd0e6452958a6fe88e1
Ascendzor <wow.troymare@gmail.com>
2018-08-18 03:25:45 +1200

    mandelbrot rendering on the map now

--- src/MandelLayer.js
+++ src/MandelLayer.js
@@ -3,46 +3,33 @@ import { MapLayer, withLeaflet } from 'react-leaflet'
 import L from 'leaflet'
 import createMandelbrotImage from './createMandelbrotImage'
 import context from './context'
+import _ from 'lodash'
 
-L.TileLayer.MandelbrotLayer = L.TileLayer.extend({
-    createTile: (coords, done) => {
-      console.log('creating tile')
-      // return document.createElement('div').appendChild(document.createTextNode('hi'))
-      // // console.log(coords)
-      // // const canvas = document.createElement('canvas')
-      // // const ctx = canvas.getContext('2d')
-      // //
-      // // let imgData = ctx.createImageData(256, 256)
-      // // canvas.width = imgData.width
-      // // canvas.height = imgData.height
-      // // imgData = createMandelbrotImage(imgData, 256, 256)
-      // // // ctx.putImageData(imgData, 0, 0)
-      // // console.log(canvas)
-      // // ctx.fillStyle = 'black'
-      // // ctx.fillRect(0, 0, 50, 50)
-      // // return canvas
-      var tile = document.createElement('canvas');
+L.MandelbrotLayer = L.GridLayer.extend({
+  createTile: (coords, done) => {
+    const tile = document.createElement('canvas')
+    const tileSize = 256
+    tile.width = tile.height = tileSize
+    const ctx = tile.getContext('2d')
+    const imgData = context().createImageData(tileSize, tileSize)
+    const mandelImage = createMandelbrotImage(imgData, tileSize, tileSize)
+    ctx.putImageData(mandelImage, 0, 0)
 
-      var tileSize = 256;
-      tile.setAttribute('width', tileSize.x);
-      tile.setAttribute('height', tileSize.y);
+    context().putImageData(mandelImage, 0, 0)
 
-      var ctx = tile.getContext('2d');
+    setTimeout(() => {
+      console.log(tile)
+      ctx.putImageData(mandelImage, 0, 0)
+      done(null, tile)
+    }, 16)
 
-      // Draw whatever is needed in the canvas context
-      // For example, circles which get bigger as we zoom in
-      ctx.beginPath();
-      ctx.arc(tileSize.x/2, tileSize.x/2, 4 + coords.z*4, 0, 2*Math.PI, false);
-      ctx.fill();
-
-      return tile;
-    }
+    return tile
+  }
 })
 
 class MandelLayer extends MapLayer {
-  createLeafletElement(a, b, c) {
-    console.log(a, b, c)
-    return new L.TileLayer.MandelbrotLayer(a, b, c)
+  createLeafletElement(a) {
+    return new L.MandelbrotLayer(a)
   }
 }
 
--- src/Mandelbrot.js
+++ src/Mandelbrot.js
@@ -9,8 +9,8 @@ console.log(MandelLayer)
 
 class Mandelbrot extends Component {
   componentDidMount() {
-    let imgData = context().createImageData(256, 256)
-    context().putImageData(createMandelbrotImage(imgData, 256, 256), 0, 0)
+    // let imgData = context().createImageData(256, 256)
+    // context().putImageData(createMandelbrotImage(imgData, 256, 256), 0, 0)
   }
   render() {
     console.log(MandelLayer)
@@ -18,11 +18,24 @@ class Mandelbrot extends Component {
       <Map
         style={{height: 400}}
         crs={Leaflet.CRS.Simple}
+        continuousWorld={true}
         bounds={[[0, 0], [1000, 1000]]}
       >
-
         <MandelLayer
+          minZoom={0}
+          maxZoom={25}
+          bounds={[[0, 0], [1000, 1000]]}
           continuousWorld={true}
+          noWrap={true}
+          tileSize={250}
+          crs={Leaflet.CRS.Simple}
+          detectRetina={false}
+          onTileloadstart={e => {
+            console.log(e)
+          }}
+          onTileload={e => {
+            console.log(e)
+          }}
         />
       </Map>
       <canvas id={'mandelbrot'} width={256} height={256} />
