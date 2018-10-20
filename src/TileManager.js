b56d00708db7808405d689a9a094cae6aa1f460c
Ascendzor <wow.troymare@gmail.com>
2018-10-20 18:40:03 +1300

    1tile 1thread

--- src/TileManager.js
+++ src/TileManager.js
@@ -2,16 +2,25 @@ import TileCreator from './TileCreator.worker'
 import PubSub from 'pubsub-js'
 import {isEqual} from 'lodash'
 
-window.tileCreator = new TileCreator()
-window.tileCreator.addEventListener('message', event => {
-  const {coords, iterations} = event.data
-  PubSub.publish('tileLoaded', event.data)
-})
 
+let tiles = []
+
+const getTileKey = coords => {
+  return Object.values(coords).join(' ')
+}
 export const getIterationsForTile = ({coords, xBounds, yBounds, tileSize, maxIterations}) => new Promise((resolve, reject) => {
+  const tile = tiles[getTileKey(coords)]
+  if(tile) return resolve(tile)
+
+  window.tileCreator = new TileCreator()
   window.tileCreator.postMessage({coords, xBounds, yBounds, tileSize, maxIterations})
+  window.tileCreator.addEventListener('message', event => {
+    const {coords, iterations} = event.data
+    PubSub.publish('tileLoaded', event.data)
+  })
   PubSub.subscribe('tileLoaded', (msg, data) => {
     if(isEqual(data.coords, coords)) {
+      tiles[getTileKey(data.coords)] = data.iterations
       resolve(data.iterations)
     }
   })
