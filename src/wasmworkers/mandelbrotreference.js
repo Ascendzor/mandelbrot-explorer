e4d2b08594c808eacfb2df3446fb0cb9844b0b67
Ascendzor <wow.troymare@gmail.com>
2019-10-29 21:04:01 -0700

    moved tilelogic into worker

--- src/wasmworkers/mandelbrotreference.js
+++ src/wasmworkers/mandelbrotreference.js
@@ -1,15 +1,29 @@
+import {tileSize} from '../constants'
 
-export default (max_iter, real, imaginary) => {
-    let zrzi = (real, imaginary);
-    let iter = 0;
-    
-    while (((zrzi[0]*zrzi[0]) + (zrzi[1]*zrzi[1]) <= 4.0) && (iter < max_iter)) {
-        zrzi = (
-            ((zrzi[0]*zrzi[0]) - (zrzi[1]*zrzi[1]) + real),
-            ((2.0 * zrzi[0] * zrzi[1]) + imaginary)
-        );
-        iter = iter + 1;
-    }
+export default (coords, max_iter, xBounds, yBounds) => {
+    let iterations = []
 
-    return iter
+    Array.from({length: tileSize}).forEach(y => Array.from({length: tileSize}).forEach(x => {
+        const preNormalizedPixel = coords.x + (x/tileSize)
+        const rangePercentile = ((preNormalizedPixel-xBounds.min) * 100) / (xBounds.max - xBounds.min)
+
+        const ypreNormalizedPixel = coords.y + (y/tileSize)
+        const yrangePercentile = ((ypreNormalizedPixel-yBounds.min) * 100) / (yBounds.max - yBounds.min)
+        const real = (rangePercentile * (1 - -2) / 100) + -2
+        const imaginary = (yrangePercentile * (1 - -1) / 100) + -1
+
+        let zrzi = (real, imaginary)
+        let iter = 0
+        
+        while (((zrzi[0]*zrzi[0]) + (zrzi[1]*zrzi[1]) <= 4.0) && (iter < max_iter)) {
+            zrzi = (
+                ((zrzi[0]*zrzi[0]) - (zrzi[1]*zrzi[1]) + real),
+                ((2.0 * zrzi[0] * zrzi[1]) + imaginary)
+            )
+            iter = iter + 1
+        }
+        iterations.push(iter)
+    }))
+
+    return iterations
 }
--- src/wasmworkers/workers.js
+++ src/wasmworkers/workers.js
@@ -8,25 +8,16 @@ onmessage = message => new Promise((resolve, reject) => {
 })
 
 const work = async () => {
-    const {coords, xBounds, yBounds, tileSize, maxIterations, computeOption} = taskQueue.pop()
+    const {coords, xBounds, yBounds, maxIterations, computeOption} = taskQueue.pop()
     rusty.then(rustyapi => {
-        let iterations = []
-        for(let y=0; y<tileSize; y++) for(let x=0; x<tileSize; x++) {
-            const preNormalizedPixel = coords.x + (x/tileSize)
-            const rangePercentile = ((preNormalizedPixel-xBounds.min) * 100) / (xBounds.max - xBounds.min)
 
-            const ypreNormalizedPixel = coords.y + (y/tileSize)
-            const yrangePercentile = ((ypreNormalizedPixel-yBounds.min) * 100) / (yBounds.max - yBounds.min)
-            const real = (rangePercentile * (1 - -2) / 100) + -2
-            const imaginary = (yrangePercentile * (1 - -1) / 100) + -1
-
-            let iteration = 0
-            
-            if(computeOption === 'js') iteration = mandelbrotreference.default(maxIterations*coords.z, real, imaginary)
-            else iteration = rustyapi.mandelbrot(maxIterations*coords.z, real, imaginary)
-
-            iterations.push(iteration)
+        let iterations = null
+        if(computeOption === 'js') {
+            iterations = mandelbrotreference.default(coords, maxIterations, xBounds, yBounds)
+        } else {
+            iterations = rustyapi.mandelbrot(coords, maxIterations, xBounds, yBounds)
         }
+        
         postMessage({
             coords,
             iterations
