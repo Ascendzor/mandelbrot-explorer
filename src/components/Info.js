e8ad18c2290b3232260c540527245411297b522f
Ascendzor <wow.troymare@gmail.com>
2019-11-02 14:02:28 -0700

    removed old map component

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {FaBitcoin} from 'react-icons/fa'
 
-class Info extends Component {
-  constructor() {
-    super()
-  }
-  render() {
-    return <div className={'Share'} onClick={e => this.props.onClick()} style={{
-      width: 40,
-      height: 40,
-      borderRadius: 10,
-      padding: 10,
-      paddingTop: 6,
-      fontSize: 12,
-      color: 'white'
-    }}>
-      Info
-      <FaBitcoin style={{
-        paddingTop: 1,
-        width: 30,
-        height: 30
-      }}/>
-    </div>
-  }
+export default ({onClick}) => {
+  return <div className={'Share'} onClick={onClick} style={{
+    width: 40,
+    height: 40,
+    borderRadius: 10,
+    padding: 10,
+    paddingTop: 6,
+    fontSize: 12,
+    color: 'white'
+  }}>
+    Info
+    <FaBitcoin style={{
+      paddingTop: 1,
+      width: 30,
+      height: 30
+    }}/>
+  </div>
 }
-
-export default Info
